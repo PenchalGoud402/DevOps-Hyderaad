2fe1b4bb25827c3dbaff7d15081b198473ec70b3
PenchalGoud402 <118295985+PenchalGoud402@users.noreply.github.com>
2022-11-15 23:14:42 +0530

    Create Remotefile.java

---
+++ Remotefile.java
@@ -0,0 +1,10 @@
+this is java file created by penchal..... practicing new thing in git. and practiced  so many git command like git add , git commit 
+ git push - u origin master, git pull, git fetch, git diff commitid1 commit2, git log, git show commitid, git status.
+   and also cofigire the global varibale like  git config --global user.name "PenchalGoud402@"
+                                               git config --global user.email "Penchal.dps@gmail.com"
+  and  i can create  multiple branches in git like git branch "branch name" we have two branches which is master and bug1 branch. if we want to push your code to remote
+  repositary. you should be in master branch only.
+    
+  i was push my files local repositary to remote repository by using username and token Creds. if you want to see those token creds you should go to setting, Devloper
+  setting and select personal  token access and click Generate token
+  
